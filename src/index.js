5215af2fe5d9dcf2f16922fd5cb0df059f7e0b91
Camilo Mendoza <camendoza94@gmail.com>
2017-03-06 22:23:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,24 +4,5 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import SmartPlanner from './components/SmartPlanner';
-
-//import reducers from './reducers';
-/*
-const logger = store => next => action => {
-  console.group(action.type);
-  console.info('dispatching', action);
-  let result = next(action);
-  console.log('next state', store.getState());
-  console.groupEnd(action.type);
-  return result;
-}
-
-const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <SmartPlanner />
-  </Provider>
-  , document.querySelector('.container'));
-*/
+//Eliminar los comentarios de código
 ReactDOM.render(<SmartPlanner />, document.querySelector('.container'));
